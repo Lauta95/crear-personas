15f10ea931bc5d8142bc6e3f90cbcbee2a0402a3
Lautaro Ezequiel Sanabria <lautaro95ss@gmail.com>
2023-05-28 18:01:30 -0300

    practica de instancias

--- funciones.js
+++ funciones.js
@@ -6,25 +6,29 @@ class Persona {
     }
 
     saludar() {
-        console.log(`El nombre de la persona es ${this.nombre} y su edad es ${this.edad}`);
+        console.log (`El nombre de la persona es ${this.nombre} y su edad es ${this.edad}`);
     }
-}
-
-function boton() {
-    let x = document.createElement("p");
-    let t = document.createTextNode(`${persona1.saludar()}`)
-    x.appendChild(t);
-    document.body.appendChild(x)
-    persona1.saludar();
-    persona2.saludar();
 }
 
 const persona1 = new Persona('Lautaro', 28);
 const persona2 = new Persona('Germán', 50);
 
+
+function boton() {
+    let x = document.createElement("p");
+    let t = document.createTextNode(`hola ${persona1}`)
+    x.appendChild(t);
+    document.body.appendChild(x)
+
+    console.log('------------------------------------------');
+    persona1.saludar();
+    persona2.saludar();
+}
+
+
 console.log(persona1.nombre, persona1.edad);
 console.log(persona2.nombre, persona2.edad);
 
-console.log('----------------------------');
-
+let instancia = new Persona('prueba de instancia', 333);
+console.log(instancia.nombre, instancia.edad);
 
