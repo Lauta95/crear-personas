9d496ca8018e3cd25cf2bd7f9dca7df9c075253f
Lautaro Ezequiel Sanabria <lautaro95ss@gmail.com>
2023-05-29 00:32:45 -0300

    datos por input y se muestra en consola

--- funciones.js
+++ funciones.js
@@ -12,24 +12,32 @@ class Persona {
 
 const persona1 = new Persona('Lautaro', 28);
 const persona2 = new Persona('Germán', 50);
+let instancia = new Persona('prueba de instancia', 333);
 
 let personaMostrar = persona1.saludar();
 
 function boton() {
+    let input1 = document.getElementById('name');
+    let input2 = document.getElementById('age');
+    let valor = input1.value;
+    let valor2 = input2.value;
+    console.log('El nombre es ' + valor + ' y su edad es de ' + valor2);
+
+
     let x = document.createElement("p");
-    let t = document.createTextNode(`hola ${personaMostrar}`)
+    let t = document.createTextNode(`hola -> ${personaMostrar}`)
     x.appendChild(t);
     document.body.appendChild(x)
 
-    console.log('------------------------------------------');
-    persona1.saludar();
-    persona2.saludar();
+
+    
+    // persona1.saludar();
+    // persona2.saludar();
+
 }
 
-
-console.log(persona1.nombre, persona1.edad);
-console.log(persona2.nombre, persona2.edad);
-
-let instancia = new Persona('prueba de instancia', 333);
-console.log(instancia.nombre, instancia.edad);
+// console.log(persona1.nombre, persona1.edad);
+// console.log(persona2.nombre, persona2.edad);
+// console.log('------------------------------------------');
+// console.log(instancia.nombre, instancia.edad);
 
